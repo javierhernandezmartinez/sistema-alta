18d6d9e1fe4eeb43811222ee62720d8febb561d5
Javier Hernandez Martinez <javier_9509@hotmail.com>
2023-06-28 00:35:42 -0600

    build

--- src/Components/BarMenu/BarMenu.jsx
+++ src/Components/BarMenu/BarMenu.jsx
@@ -6,27 +6,27 @@ const BarMenu = () => {
         {
             label: 'Home',
             icon: 'pi pi-fw pi-file',
-            command: () => { window.open("/", "_self"); },
+            command: () => { window.open("#/", "_self"); },
         },
         {
             label: 'Registrate',
             icon: 'pi pi-fw pi-calendar',
-            command: () => { window.open("/register", "_self"); },
+            command: () => { window.open("#/register", "_self"); },
         },
         {
             label: 'Login',
             icon: 'pi pi-fw pi-power-off',
-            command: () => { window.open("/login", "_self"); },
+            command: () => { window.open("#/login", "_self"); },
         },
         {
             label: 'Perfil',
             icon: 'pi pi-fw pi-power-off',
-            command: () => { window.open("/perfil", "_self"); },
+            command: () => { window.open("#/perfil", "_self"); },
         },
         {
             label: 'Administaciòn',
             icon: 'pi pi-fw pi-power-off',
-            command: () => { window.open("/administration", "_self"); },
+            command: () => { window.open("#/administration", "_self"); },
         }
     ];
     const start = <img alt="logo" src="https://primefaces.org/cdn/primereact/images/logo.png" height="40" className="mr-2"></img>;
