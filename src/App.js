c2dc25596be7ab1d1f52812266015a58a6f85bc4
Javier Hernandez Martinez <javier_9509@hotmail.com>
2023-06-27 22:45:37 -0600

    homepage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {BrowserRouter, Routes, Route, HashRouter} from "react-router-dom";
 import './App.css';
 import Home from "./Modules/Home/Home"
 import Login from "./Modules/Login/Login";
